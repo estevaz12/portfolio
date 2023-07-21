882092a453722aa084f0067318fe16b7c03bb05e
Esteban Vazquez Cardona <estebanvazquez1607@gmal.com>
2023-07-21 10:55:29 -0300

    working on sidebar

--- app/components/Sidebar.js
+++ app/components/Sidebar.js
@@ -1,4 +1,9 @@
 import Link from 'next/link';
+import Projects from '../icons/projects.svg';
+import Skills from '../icons/skills.svg';
+import About from '../icons/about.svg';
+import Contact from '../icons/contact.svg';
+import OutLink from '../icons/out-link.svg';
 import Resume from '../icons/resume.svg';
 import Lang from '../icons/lang.svg';
 import Dark from '../icons/dark.svg';
@@ -11,29 +16,40 @@ export default function Sidebar() {
         <div>
           <ul className='mx-0'>
             {pages.map((page) => (
-              <li key={page}>
+              <li key={page.name}>
                 <Link
-                  href={`/unlocked/${page.toLowerCase()}`}
+                  href={`/unlocked/${page.name.toLowerCase()}`}
                   className='text-green hover:bg-[var(--primary-hover)] hover:text-coffee rounded-none pl-4'
                 >
-                  {page}
+                  {page.icon} {page.name}
                 </Link>
               </li>
             ))}
             <li>
+              <hr />
+            </li>
+            <li>
               <Link
                 href='#'
-                className='text-green hover:bg-[var(--primary-hover)] hover:text-coffee rounded-none pl-4 border-t border-[var(--muted-border-color)]'
+                className='text-green hover:bg-[var(--primary-hover)] hover:text-coffee rounded-none pl-4'
               >
-                Church
+                Church&nbsp;&nbsp;
+                {
+                  <OutLink
+                    color='#54DEFD'
+                    className='w-[var(--font-size)] h-[var(--font-size)]'
+                  />
+                }
               </Link>
             </li>
           </ul>
         </div>
 
         <div>
-          <hr />
           <ul className='mx-0'>
+            <li>
+              <hr />
+            </li>
             {options.map((option) => (
               <li key={option.name}>
                 <Link
@@ -53,22 +69,59 @@ export default function Sidebar() {
   );
 }
 
-const pages = ['Projects', 'Skills', 'About', 'Contact'];
+const pages = [
+  {
+    name: 'Projects',
+    icon: (
+      <Projects
+        color='#E2124D'
+        className='w-[var(--font-size)] h-[var(--font-size)]'
+      />
+    ),
+  },
+  {
+    name: 'Skills',
+    icon: (
+      <Skills
+        color='#FFB10A'
+        className='w-[var(--font-size)] h-[var(--font-size)]'
+      />
+    ),
+  },
+  {
+    name: 'About',
+    icon: (
+      <About
+        color='#CC4BC2'
+        className='w-[var(--font-size)] h-[var(--font-size)]'
+      />
+    ),
+  },
+  {
+    name: 'Contact',
+    icon: (
+      <Contact
+        color='#F9EBE0'
+        className='w-[var(--font-size)] h-[var(--font-size)]'
+      />
+    ),
+  },
+];
 
 const options = [
   {
     name: 'Resume',
     href: '#',
-    icon: <Resume width='0.875em' height='0.875em' />,
+    icon: <Resume color='#BF00FF' width='0.875em' height='0.875em' />,
   },
   {
     name: 'Español',
     href: '#',
-    icon: <Lang width='0.875em' height='0.875em' />,
+    icon: <Lang color='#54DEFD' width='0.875em' height='0.875em' />,
   },
   {
     name: 'Theme',
     href: '#',
-    icon: <Dark width='0.875em' height='0.875em' />,
+    icon: <Dark color='#FFB10A' width='0.875em' height='0.875em' />,
   },
 ];
--- app/unlocked/layout.js
+++ app/unlocked/layout.js
@@ -3,11 +3,11 @@ import Sidebar from '../components/Sidebar';
 export default function UnlockedLayout({ children }) {
   return (
     <div className='container-fluid h-screen overflow-hidden py-[var(--block-spacing-vertical)]'>
-      <div className='flex h-full max-w-screen-xl mx-auto my-0 shadow bg-coffee rounded-[var(--border-radius)]'>
-        <div className='h-full border-r basis-1/6'>
+      <div className='flex h-full max-w-screen-xl mx-auto my-0 shadow rounded-[var(--border-radius)]'>
+        <div className='h-full basis-1/6 bg-coffee/90 backdrop-blur-sm'>
           <Sidebar />
         </div>
-        <div className='h-full basis-5/6 pt-[var(--typography-spacing-vertical)] container-fluid'>
+        <div className='h-full basis-5/6 pt-[var(--typography-spacing-vertical)] container-fluid bg-coffee/80 backdrop-blur-sm'>
           {children}
         </div>
       </div>
