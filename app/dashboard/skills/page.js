f66073ddf4231140543195ec85f3c732c40d70a4
Esteban Vazquez Cardona <estebanvazquez1607@gmal.com>
2023-08-07 17:06:44 -0300

    working on skills page

---
+++ app/dashboard/skills/page.js
@@ -0,0 +1,19 @@
+import Header from '@/components/Header';
+import SkillCard from '@/components/SkillCard';
+import { getSkillProjectCount } from '@/lib/skills';
+
+const skills = getSkillProjectCount();
+
+export default function Skills() {
+  return (
+    <>
+      <Header title={'Skills'} isSearchEnabled={true} />
+
+      <div className='grid grid-cols-3 gap-2'>
+        {skills.map((skill) => (
+          <SkillCard key={skill.id} skill={skill} />
+        ))}
+      </div>
+    </>
+  );
+}
---
+++ components/SkillCard.js
@@ -0,0 +1,23 @@
+import StackSvg from './StackSvg';
+import Link from 'next/link';
+
+export default function SkillCard({ skill }) {
+  return (
+    <article className='m-0'>
+      <div className='flex items-center gap-2 mb-[var(--typography-spacing-vertical)]'>
+        <StackSvg skill={skill.id} className='w-8 h-8' />
+        <h4 className='m-0'>{skill.name}</h4>
+      </div>
+
+      <Link
+        href='#'
+        className='focus:bg-transparent focus:text-[var(--primary)] focus:no-underline hover:bg-transparent hover:text-[var(--primary)] hover:no-underline'
+      >
+        <span className='inline-block w-6 h-6 text-center text-white rounded-full bg-mid-blue'>
+          {skill.projects}
+        </span>{' '}
+        <span className='focus:underline hover:underline'>Projects</span>
+      </Link>
+    </article>
+  );
+}
