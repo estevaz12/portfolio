937e96bd67c156c1302e109e31042c251594a1d8
Esteban Vazquez <esteban.vazquez@techbrainscorp.com>
2023-08-21 13:14:57 -0300

    debugging gallery 18

--- components/Gallery.js
+++ components/Gallery.js
@@ -7,20 +7,18 @@ export default async function Gallery({ page, post }) {
   console.log(images);
 
   return (
-    images.gallery.length > 0 && (
-      <div className='gap-2 space-y-4 sm:space-y-2 sm:columns-2 animate-fade-in'>
-        {images.gallery.map((img) => (
-          <Image
-            key={img.id}
-            src={img.src}
-            alt={img.alt}
-            width={1440}
-            height={900}
-            className='w-full h-auto rounded-[var(--border-radius)]'
-          />
-        ))}
-      </div>
-    )
+    <div className='gap-2 space-y-4 sm:space-y-2 sm:columns-2 animate-fade-in'>
+      {images.gallery.map((img) => (
+        <Image
+          key={img.id}
+          src={img.src}
+          alt={img.alt}
+          width={1440}
+          height={900}
+          className='w-full h-auto rounded-[var(--border-radius)]'
+        />
+      ))}
+    </div>
   );
 }
 
