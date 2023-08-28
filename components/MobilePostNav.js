a69dadf9d90e469364aea8c29e610e7b19375f3e
Esteban Vazquez <esteban.vazquez@techbrainscorp.com>
2023-08-28 10:43:43 -0300

    fixing #content issue 5

--- components/MobilePostNav.js
+++ components/MobilePostNav.js
@@ -13,8 +13,8 @@ export default function MobilePostNav() {
     <div className='flex items-center justify-between'>
       <Back color='#a2afb9' className='w-8 h-8' onClick={() => router.back()} />
 
-      <div className='flex gap-4 w-full items-center justify-end'>
-        <Link href='#menu' className='sm:hidden'>
+      <div className='flex items-center justify-end w-full gap-4'>
+        <Link href='#menu'>
           <Menu color='#a2afb9' className='w-5 h-5' />
         </Link>
 
